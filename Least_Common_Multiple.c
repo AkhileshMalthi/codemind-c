62e4f2e58bb96927bd8d0bd02dbbcacb85c42b18
Akhilesh Malthi <119729949+AkhileshMalthi@users.noreply.github.com>
2022-12-16 14:37:50 +0530

    #include<stdio.h> int main() {     int n,m;     scanf("%d%d",&n,&m);     for (int i=1;i<=n*m;i++) {             if (n*i%m==0) {                 printf("%d",n*i);                 break;             }     }     return 0; }

--- Least_Common_Multiple.c
+++ Least_Common_Multiple.c
@@ -3,10 +3,10 @@ int main() {
     int n,m;
     scanf("%d%d",&n,&m);
     for (int i=1;i<=n*m;i++) {
-    if (n*i%m==0) {
-    printf("%d",n*i);
-    break;
-    }
+            if (n*i%m==0) {
+                printf("%d",n*i);
+                break;
+            }
     }
     return 0;
 }
