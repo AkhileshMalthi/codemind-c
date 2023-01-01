f81cef8e08931ad3505b47f164c1daccfa578a82
Akhilesh Malthi <119729949+AkhileshMalthi@users.noreply.github.com>
2023-01-01 07:16:25 +0530

    #include<stdio.h> int main() {     int n;     float sum=0;     scanf("%d",&n);     int a[n];     for (int i=0;i<n;i++) {         scanf("%d",&a[i]);     }     for (int i=0;i<n;i++) {         sum+=a[i];     }     printf("%.2f",sum/n);     return 0; }

---
+++ average_of_the_array.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main() {
+    int n; 
+    float sum=0;
+    scanf("%d",&n);
+    int a[n];
+    for (int i=0;i<n;i++) {
+        scanf("%d",&a[i]);
+    }
+    for (int i=0;i<n;i++) {
+        sum+=a[i];
+    }
+    printf("%.2f",sum/n);
+    return 0;
+}
