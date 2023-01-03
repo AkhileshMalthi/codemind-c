12b5ae3d60e5be91a00689d2bb6d0d6247dda5fc
Akhilesh Malthi <119729949+AkhileshMalthi@users.noreply.github.com>
2023-01-03 20:19:44 +0530

    #include<stdio.h> int main() {     int n,evensum=0,oddsum=0;     scanf("%d",&n);     int a[n];     for (int i=0;i<n;i++) {         scanf("%d",&a[i]);         if (i%2==0)         evensum+=a[i];         else         oddsum+=a[i];     }     evensum>oddsum ? printf("%d",evensum-oddsum):printf("%d",oddsum-evensum);     return 0; }

---
+++ Absolute_difference_b/w_sum_of_even_and_sum_of_odd_indexed_elements.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main() {
+    int n,evensum=0,oddsum=0;
+    scanf("%d",&n);
+    int a[n];
+    for (int i=0;i<n;i++) {
+        scanf("%d",&a[i]);
+        if (i%2==0)
+        evensum+=a[i];
+        else
+        oddsum+=a[i];
+    }
+    evensum>oddsum ? printf("%d",evensum-oddsum):printf("%d",oddsum-evensum);
+    return 0;
+}
